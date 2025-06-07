32b48268349b531a537ee09714aaac5bf60c3a2b
raja9661 <rajababu.king1@gmail.com>
2025-06-07 14:19:08 +0530

    cors

--- index.js
+++ index.js
@@ -26,31 +26,58 @@ const server = http.createServer(app);
 const io = socket.init(server);
 
 
+// const allowedOrigins = [
+//   'http://localhost:3000',
+//   'http://127.0.0.1:3000',
+//   'http://localhost:5173',
+//   'http://localhost:8080', 
+//   'https://www.rvsdoc.com',
+//   'http://13.126.216.112:5173',
+//   'http://127.0.0.1:8080'
+  
+// ];
+
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin) return callback(null, true); //Allow non-browser tools like Postman
+//     if (allowedOrigins.includes(origin)) {
+//       return callback(null, true);
+//     } else {
+//       return callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+// }));
 const allowedOrigins = [
   'http://localhost:3000',
-  'http://127.0.0.1:3000',
+  'http://127.0.0.1:3000', 
   'http://localhost:5173',
-  'http://localhost:8080', 
+  'http://localhost:8080',
   'https://www.rvsdoc.com',
   'http://13.126.216.112:5173',
   'http://127.0.0.1:8080'
-  
 ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true); //Allow non-browser tools like Postman
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
+// Enhanced CORS configuration
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  console.log('Request Origin:', origin); // Debug log
+  
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
     }
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-
+  }
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
